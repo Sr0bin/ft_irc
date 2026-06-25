78b71b33730f29d37f1cd9727c6e1d151bab6c70
Pierre-Antoine Rigaudier <prigaudi@z1r11p5.42lyon.fr>
2026-06-25 15:30:04 +0200

    fix(CommandDispatcher): registerCommand(Kick)

--- server/src/commands/CommandDispatcher.cpp
+++ server/src/commands/CommandDispatcher.cpp
@@ -6,7 +6,7 @@
 /*   By: prigaudi <prigaudi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 16:38:12 by rorollin          #+#    #+#             */
-/*   Updated: 2026/06/25 11:09:12 by prigaudi         ###   ########.fr       */
+/*   Updated: 2026/06/25 15:29:20 by prigaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ CommandDispatcher::CommandDispatcher(Server &server) : _server(server) {
 	registerCommand("join", new JoinCommand(_server));
 	registerCommand("part", new PartCommand(_server));
 	registerCommand("topic", new TopicCommand(_server));
-	registerCommand("kick", new TopicCommand(_server));
+	registerCommand("kick", new KickCommand(_server));
 	registerCommand("mode", new ModeCommand(_server));
 	registerCommand("ping", new PingCommand(_server));
 	registerCommand("privmsg", new PrivmsgCommand(_server));
